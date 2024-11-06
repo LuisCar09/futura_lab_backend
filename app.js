50dc3cc1dde810bdab20f2eaaabba9d535c56b40
Luis Carlos <luiscbravo94@gmail.com>
2024-11-06 23:19:51 +0000

    feat: connected DB

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import dbConnection from './config/db.js'
 dotenv.config()
+import User from './models/User.js'
 
 const app = express()
 const PORT = process.env.PORT
@@ -9,7 +10,12 @@ const PORT = process.env.PORT
 app.get('/',(req,res) => {
     res.send('<h1>Server On</h1>')
 })
-
+app.post('/createUser',(req,res)=>{
+    const data = req.body
+    console.log(data);
+    res.status(201).json({success:"User Create Sucessfully"})
+    
+})
 app.listen(PORT,()=> {
     console.log(`Server listening on http://localhost:${PORT}/`)
 })
