b9512e6494c1e28876945faa8e24f92b12d0524d
Luis Carlos <luiscbravo94@gmail.com>
2025-03-10 16:19:04 +0100

    feat: fixed logic at gerUserEmail

--- controllers/authUsersControllers.js
+++ controllers/authUsersControllers.js
@@ -58,13 +58,10 @@ const authUsersControllers = {
     getUserEmail: async(req,res) => {
         try {
             const {email} = req.params
-            
-            
-            
+                
             const user = await User.findOne({email})
-            
-            
-            res.status(200).json(user)
+        
+            res.status(200).json({user})
         } catch (error) {
             console.error(error.message)
         }
